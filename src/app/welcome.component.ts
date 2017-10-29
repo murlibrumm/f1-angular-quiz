512a50c0f44bb3ab9b6d03b4e036c31cb9da9867
Wolfgang Weintritt <wolfgang.weintritt@gmail.com>
2017-10-29 23:47:58 +0100

    Improve some cosmetic stuff

--- src/app/welcome.component.ts
+++ src/app/welcome.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-welcome',
   template: `
-    <p>Welcome to F1-Quizzes! Choose one of the quizzes to start, or dig into statistics :)</p>
+    <p style='padding: 20px; font-size: 1.5em'>Welcome to F1-Quizzes! Choose one of the quizzes to start! :)</p>
   `
 })
 
